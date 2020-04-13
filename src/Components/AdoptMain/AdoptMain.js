4a755dca86c0e15b8596ae5228a12affe2fddf05
Christopher Floyd <cfloyd252@gmail.com>
2020-04-13 00:04:13 -0700

    finished

--- src/Components/AdoptMain/AdoptMain.js
+++ src/Components/AdoptMain/AdoptMain.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PetfulApiService from '../../services/petful-api-service'
-//import './AdoptMain.css'
+import './AdoptMain.css'
 
 export default class AdoptMain extends React.Component {
 
@@ -9,6 +9,7 @@ export default class AdoptMain extends React.Component {
     dogs: [],
     cats: [],
     people: [],
+    currentUser: ''
   }
 
   componentDidMount(){
@@ -27,7 +28,66 @@ export default class AdoptMain extends React.Component {
       .then(res => this.setState({ people: res }))
       .catch(res => this.setState({ error: res.error }))
 
+    // setInterval(() => {
+    //   let randomNumber = Math.floor(Math.random() * 2) + 1
+    //   console.log(randomNumber);
+      
+    //   if(randomNumber === 1) {
+    //     PetfulApiService.deletePetAndPerson('dog')
+    //       .then( res => {
+    //         PetfulApiService.getPets()
+    //           .then(res => this.setState({ dogs: res.dogs }))
+    //           .catch(res => this.setState({ error: res.error }))
+    //         PetfulApiService.getPeople()
+    //           .then(res => this.setState({ people: res }))
+    //           .catch(res => this.setState({ error: res.error }))
+    //       })
+    //       .catch(res => this.setState({ error: res.error }))
+    //   } else {
+    //     PetfulApiService.deletePetAndPerson('cat')
+    //       .then( res => {
+    //         PetfulApiService.getPets()
+    //           .then(res => this.setState({ cats: res.cats }))
+    //           .catch(res => this.setState({ error: res.error }))
+    //         PetfulApiService.getPeople()
+    //           .then(res => this.setState({ people: res }))
+    //           .catch(res => this.setState({ error: res.error }))
+    //       })
+    //       .catch(res => this.setState({ error: res.error }))
+    //   }
+    // }, 5000);
+  }
+
+  incrementPerson() {
+    let randomNumber = Math.floor(Math.random() * 2) + 1
+    console.log(randomNumber);
+    let petType = ''
+    
+    if(randomNumber === 1) {
+      petType = 'dog'
+    } else {
+      petType = 'cat'
+    }
+
     setInterval(() => {
+      console.log('every 5 seconds')
+    }, 5000);
+  }
+
+  handleAddName(e) {
+    e.preventDefault()
+    const { name } = e.target
+    PetfulApiService.postPerson(name.value)
+      .then( res => {
+        name.value = ''
+        this.setState({
+          people: [...this.state.people, res],
+          currentUser: res
+        })
+      })
+      .catch(res => this.setState({ error: res.error }))
+
+    this.timeout = setInterval(() => {
       let randomNumber = Math.floor(Math.random() * 2) + 1
       console.log(randomNumber);
       
@@ -55,36 +115,6 @@ export default class AdoptMain extends React.Component {
           .catch(res => this.setState({ error: res.error }))
       }
     }, 5000);
-  }
-
-  incrementPerson() {
-    let randomNumber = Math.floor(Math.random() * 2) + 1
-    console.log(randomNumber);
-    let petType = ''
-    
-    if(randomNumber === 1) {
-      petType = 'dog'
-    } else {
-      petType = 'cat'
-    }
-
-    setInterval(() => {
-      console.log('every 5 seconds')
-    }, 5000);
-  }
-
-  handleAddName(e) {
-    e.preventDefault()
-    const { name } = e.target
-    PetfulApiService.postPerson(name.value)
-      .then( res => {
-        name.value = ''
-        this.setState({people: [...this.state.people, res]})
-      })
-      .catch(res => this.setState({ error: res.error }))
-
-    // this.renderAdoptDogButton(name)
-    // this.renderAdoptCatButton(name)
   }
 
   handleAdoptDog(e) {
@@ -117,25 +147,25 @@ export default class AdoptMain extends React.Component {
     // alert('Adopted a cat! Congrats!')
   }
 
-  // renderAdoptDogButton(name) {
-  //   let button
-  //   if(name === this.state.people[0]){
-  //     button = <button onClick={e => this.handleAdoptDog(e)}>
-  //       Adopt Me!
-  //     </button>
-  //   }
-  //   return button
-  // }
+  renderAdoptDogButton(name) {
+    let button
+    if(this.state.currentUser === this.state.people[0]){
+      button = <button onClick={e => this.handleAdoptDog(e)}>
+        Adopt Me!
+      </button>
+    }
+    return button
+  }
 
-  // renderAdoptCatButton(name) {
-  //   let button
-  //   if(name === this.state.people[0]){
-  //     button = <button onClick={e => this.handleAdoptCat(e)}>
-  //       Adopt Me!
-  //     </button>
-  //   }
-  //   return button
-  // }
+  renderAdoptCatButton(name) {
+    let button
+    if(this.state.currentUser === this.state.people[0]){
+      button = <button onClick={e => this.handleAdoptCat(e)}>
+        Adopt Me!
+      </button>
+    }
+    return button
+  }
 
   renderPeople() {
     let line = []
@@ -157,8 +187,27 @@ export default class AdoptMain extends React.Component {
       content = <p className='red'>There was an error</p>
     } else if (dogs.length === 0 || cats.length === 0) {
       return <div className='loading'>loading...</div>
-    } else if (people.length !== 0) {
-      // this.incrementPerson()
+    } else if (people.length === 1) {
+      clearInterval(this.timeout)
+      this.addPeople = setInterval(() => {
+        console.log('adding')
+        const firstNames = ['Kit', 'Adam', 'Keke', 'Emma', 'Howie', 'Donald']
+        const lastNames = ['Harington', 'Driver', 'Palmer', 'Roberts', 'Mandel', 'Glover']
+
+        const getRandomName = () => `${firstNames[Math.floor(Math.random() * firstNames.length)]} ${lastNames[Math.floor(Math.random() * lastNames.length)]}`
+
+        console.log(getRandomName)
+
+        PetfulApiService.postPerson(getRandomName())
+        .then( res => {
+          this.setState({
+            people: [...this.state.people, res],
+          })
+        })
+        .catch(res => this.setState({ error: res.error }))
+        }, 5000);
+    } else if (people.length === 5) {
+      clearInterval(this.addPeople)
     }
     return(
       <div className='adopt-main'>
@@ -166,30 +215,26 @@ export default class AdoptMain extends React.Component {
 
         {content}
 
-        <div className='dog-container'>
-          <img src={this.state.dogs[0].imageURL} alt={this.state.dogs[0].description}></img>
-          <h2>{this.state.dogs[0].name}</h2>
-          <p>{this.state.dogs[0].age} years old</p>
-          <p>Gender: {this.state.dogs[0].gender}</p>
-          <p>Breed: {this.state.dogs[0].breed}</p>
-          <p>How I got here: {this.state.dogs[0].story}</p>
-          <button onClick={e => this.handleAdoptDog(e)}>
-            Adopt Me!
-          </button>
-          {/* {this.renderAdoptDogButton()} */}
-        </div>
+        <div className='adoption-container'>
+          <div className='dog-container'>
+            <img className='pet-img' src={this.state.dogs[0].imageURL} alt={this.state.dogs[0].description}></img>
+            <h2>{this.state.dogs[0].name}</h2>
+            <p>{this.state.dogs[0].age} years old</p>
+            <p>Gender: {this.state.dogs[0].gender}</p>
+            <p>Breed: {this.state.dogs[0].breed}</p>
+            <p>How I got here: {this.state.dogs[0].story}</p>
+            {this.renderAdoptDogButton()}
+          </div>
 
-        <div className='cat-container'>
-          <img src={this.state.cats[0].imageURL} alt={this.state.cats[0].description}></img>
-          <h2>{this.state.cats[0].name}</h2>
-          <p>{this.state.cats[0].age} years old</p>
-          <p>Gender: {this.state.cats[0].gender}</p>
-          <p>Breed: {this.state.cats[0].breed}</p>
-          <p>How I got here: {this.state.cats[0].story}</p>
-          <button onClick={e => this.handleAdoptCat(e)}>
-            Adopt Me!
-          </button>
-          {/* {this.renderAdoptCatButton()} */}
+          <div className='cat-container'>
+            <img className='pet-img' src={this.state.cats[0].imageURL} alt={this.state.cats[0].description}></img>
+            <h2>{this.state.cats[0].name}</h2>
+            <p>{this.state.cats[0].age} years old</p>
+            <p>Gender: {this.state.cats[0].gender}</p>
+            <p>Breed: {this.state.cats[0].breed}</p>
+            <p>How I got here: {this.state.cats[0].story}</p>
+            {this.renderAdoptCatButton()}
+          </div>
         </div>
 
         <div className='queue-container'>
